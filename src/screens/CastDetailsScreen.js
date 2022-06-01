0e627ea2fc2925c80762f3523b7b95965bc998b9
Davidson-09 <davidsonakra@gmail.com>
2022-06-01 18:06:33 +0100

    added message container to cast details screen

--- src/screens/CastDetailsScreen.js
+++ src/screens/CastDetailsScreen.js
@@ -1,12 +1,57 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet, ScrollView} from 'react-native'
+import { primary } from '../assets/color'
+import { normalSize } from '../assets/textSettings'
+import UpvoteIcon from '../assets/icons/UpvoteIcon'
+import DownVoteIcon from '../assets/icons/DownVoteIcon'
 
 export function CastDetailsScreen(props) {
     
 
     return (
         <View>
-            <Text>app</Text>
+            <View style={styles.topBar}>
+                <Text style={{color:'white', fontWeight:'bold', fontSize:30}}>casted</Text>
+            </View>
+            <ScrollView>
+                <View style={styles.container}>
+                    <View style={styles.messageContainer}>
+                        <View style={{marginRight: 10}}>
+                            <Text style={styles.message}>this is a message on the cast app</Text>
+                            <Text style={styles.comments}>10 comments</Text>
+                        </View>
+                        <View style={{alignItems:'center'}}>
+                            <UpvoteIcon/>
+                            <Text style={{color:'black', fontSize:30}}>10</Text>
+                            <DownVoteIcon/>
+                        </View>
+                    </View>
+                </View>
+            </ScrollView>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    topBar:{
+        height: 60,
+        backgroundColor:primary,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    container:{
+        padding: 20
+    },
+    message:{
+        color:'black',
+        fontSize: normalSize,
+        fontWeight:'bold',
+        
+    },
+    messageContainer:{
+        flexDirection: 'row',
+        width:'100%',
+        justifyContent:'space-between',
+        marginBottom: 30
+    },
+})
