b28bb2b04f97f5490dce4e7d86f4344d8f98178a
Davidson-09 <davidsonakra@gmail.com>
2022-06-02 14:14:48 +0100

    added click event to send cast button

--- src/components/CastArea.js
+++ src/components/CastArea.js
@@ -1,17 +1,26 @@
-import React from 'react'
-import{View, StyleSheet, TextInput} from 'react-native'
+import React, {useState} from 'react'
+import{View, StyleSheet, TextInput, TouchableOpacity} from 'react-native'
 import { faintGrey } from '../assets/color'
 import SendGreenIcon from '../assets/icons/SendGreenIcon'
 import { normalSize } from '../assets/textSettings'
+import firestore from '@react-native-firebase/firestore';
 
 //cast input area
 export default function CastArea(props) {
     
+    const [message, setMessage] = useState('')
+
+    const sendCast =()=>{
+        console.log('casted')
+    }
 
     return ( 
         <View style={styles.container}>
-            <TextInput style={styles.input} placeholder='say something' placeholderTextColor={'white'}/>
-            <SendGreenIcon />
+            <TextInput style={styles.input} placeholder='say something' placeholderTextColor={'white'}
+                value={message} onChangeText={setMessage}/>
+            <TouchableOpacity onPress={sendCast}>
+                <SendGreenIcon />
+            </TouchableOpacity>
         </View>
     )
 }
