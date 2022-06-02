6d68ca671e7c4e80414d0a94d5a109556a717434
Davidson-09 <davidsonakra@gmail.com>
2022-06-02 15:00:51 +0100

    can now read messages

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {View, Text, StyleSheet, ScrollView} from 'react-native'
 import { primary } from '../assets/color'
 import Cast from '../components/Cast'
 import CastArea from '../components/CastArea'
+import firestore from '@react-native-firebase/firestore';
 
 export default function Home(props) {
-    
+
+    const db = firestore()
+
+    useEffect(()=>{
+        loadMessages()
+    },[])
+
+    const loadMessages =async()=>{
+        // load cast messages
+        db.collection('casts').orderBy('time').get().then((querySnapshot)=>{
+            let c = []; // temporary list of chats
+            querySnapshot.forEach((doc)=>{
+                let cast = {data: doc.data(), id: doc.id};
+                c.push(cast);
+            })
+            console.log(c)
+        })
+    }
 
     return (
         <View style={{height:'100%'}}>
